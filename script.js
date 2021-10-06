af655eeacaeb176978c1c9763caa191fc69bd4aa
Dan Miloch <dmiloch@gmail.com>
2021-10-06 09:42:13 -0400

    added parens

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-var tele = document.querySelector('#phone');
-
-tele.addEventListener('keyup', function(e){
-  if (event.key != 'Backspace' && (tele.value.length === 3 || tele.value.length === 7)){
-  tele.value += '-';
-  }
+document.getElementById('phone').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
